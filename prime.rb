252853fe6e6113924dc276180ac83eae6b9a4d08
Katherine Wren <kwren@outlook.com>
2019-04-10 10:31:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  Math.sqrt(num).to_i.downto(2).each
+  if num <= 1 
+    return false
+  elsif 
+   {|i| return false if num % i == 0}
+   return false
+else
+  return true
+end
